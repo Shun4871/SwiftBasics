aeb3a33d85d45f2e0f8e257dd2ce99eaef71edf8
ShunnosukeTsuge <shuntsugework@gmail.com>
2024-05-11 20:21:47 +0900

    Initial Commit

---
+++ Chapter0-はじめに.playground/Contents.swift
@@ -0,0 +1,5 @@
+
+import Foundation
+// これは、コメントです。メモなど好きなことを書くことができます。
+// import FoundationはXcodeであればimport UIKitでも同じ意味になります。
+print("Hello Swift!")
---
+++ Chapter8-関数.playground/Contents.swift
@@ -0,0 +1,9 @@
+// 8.1
+func greet() {
+    print("Hello")
+}
+
+// 8.2
+func combineAndPrint(firstName: String, lastName: String) {
+    print(firstName + lastName)
+}
